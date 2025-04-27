3178f586876e683e6fb4fc2cf9624fc517b31b9e
Md Shafi Hudewale <136341797+shafihudewale@users.noreply.github.com>
2025-04-27 11:13:37 +0530

    Create Categories.sql

---
+++ Categories.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Categories (
+  CategoryID INT PRIMARY KEY AUTO_INCREMENT,
+  CategoryName VARCHAR(255) NOT NULL,
+  Description VARCHAR(255)
+);
+
+INSERT INTO Categories (CategoryID, CategoryName, Description) VALUES
+(1, 'Beverages', 'Soft drinks, coffees, teas, beers, and ales'),
+(2, 'Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings'),
+(3, 'Confections', 'Desserts, candies, and sweet breads'),
+(4, 'Dairy Products', 'Cheeses'),
+(5, 'Grains/Cereals', 'Breads, crackers, pasta, and cereal'),
+(6, 'Meat/Poultry', 'Prepared meats'),
+(7, 'Produce', 'Dried fruit and bean curd'),
+(8, 'Seafood', 'Seaweed and fish');
